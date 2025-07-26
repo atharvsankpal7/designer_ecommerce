dc104953b7666ba1eef36f60a2f76f66c9b119f2
Atharv Sankpal <atharvsankpal799@gmail.com>
2025-07-27 04:04:02 +0530

    .

--- components/layout/ssr-header.tsx
+++ components/layout/ssr-header.tsx
@@ -1,8 +1,9 @@
+export const dynamic = "force-dynamic";
+
 import { getNavigationSections } from '@/lib/section-utils';
 import { ClientHeader } from './client-header';
 
 export async function SSRHeader() {
   const navigationSections = await getNavigationSections();
-
   return <ClientHeader navigationSections={navigationSections} />;
 }
