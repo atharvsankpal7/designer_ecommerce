5ffd822a2976fddab94a9f117835f19db3e54b38
Atharv Sankpal <atharvsankpal799@gmail.com>
2025-07-27 16:12:42 +0530

    fixed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
