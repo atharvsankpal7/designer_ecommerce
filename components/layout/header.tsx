f25de124344b18f2ec9cc7578ae44b5ba917feb2
Atharv Sankpal <atharvsankpal799@gmail.com>
2025-07-28 19:12:43 +0530

    .

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,9 +1,16 @@
 import { getNavigationSections } from '@/lib/section-utils';
+import { getHeaderData } from '@/lib/actions';
 import { ClientHeader } from './client-header';
 export const dynamic = 'force-dynamic';
 
 export async function Header() {
-  const navigationSections = await getNavigationSections();
+  const [navigationSections, headerData] = await Promise.all([
+    getNavigationSections(),
+    getHeaderData()
+  ]);
 
-  return <ClientHeader navigationSections={navigationSections} />;
+  return <ClientHeader 
+    navigationSections={navigationSections} 
+    headerData={headerData}
+  />;
 }
